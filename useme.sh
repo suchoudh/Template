7143418777fc8920667d04a5719f214de00c047d
budh <budh@localhost.localdomain>
2023-04-30 14:38:26 +0530

    added location in readme file

--- useme.sh
+++ useme.sh
@@ -1,6 +1,7 @@
-
-export DIRNAME="$(date +'%Y_%m_%d_%H_%M')_AUTO"
 export DATETAG="$(date +'%Y_%m_%d_%H_%M')"
+export DIRNAME="$DATETAG_AUTO"
+echo $DIRNAME
 mkdir $DIRNAME
 cp -R ~/Documents/Template/CopyOnlyThis "$DIRNAME"/.
 touch "$DIRNAME"/"readme$DATETAG.md"
+cat ~/Documents/Template/CopyOnlyThis/readmeDOLLARDATETAG.md >>$DIRNAME/"readme$DATETAG.md"
