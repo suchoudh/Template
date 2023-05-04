338bd8d5f369851f118837f5fc7fef4eb074ae35
suchoudh <sunil.choudhary@gmail.com>
2023-05-04 19:14:07 +0530

    v1 added

--- useme.sh
+++ useme.sh
@@ -2,6 +2,6 @@ export DATETAG="$(date +'%Y_%m_%d_%H_%M')"
 export DIRNAME=$DATETAG"_AUTO"
 echo $DIRNAME
 mkdir $DIRNAME
-cp -R ~/Documents/Template/CopyOnlyThis "$DIRNAME"/.
+cp -R ~/Documents/Template/CopyOnlyThis v1"$DIRNAME"/.
 touch "$DIRNAME"/"readme$DATETAG.md"
 cat ~/Documents/Template/CopyOnlyThis/readmeDOLLARDATETAG.md >>$DIRNAME/"readme$DATETAG.md"
