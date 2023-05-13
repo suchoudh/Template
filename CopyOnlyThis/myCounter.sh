67069b08c2103ac4c658708a410f602a46b7a877
Sunil Choudhary <sunil.choudhary@gmail.com>
2023-05-13 12:05:21 +0530

    Update myCounter.sh
    added less and more along with minutes

--- CopyOnlyThis/myCounter.sh
+++ CopyOnlyThis/myCounter.sh
@@ -9,8 +9,20 @@ while true
 do
 	echo $COUNTER seconds remaining in break
 	COUNTER=$(( COUNTER - 1 ))
-    echo Predicting task  $2  to require  $1 minutes but took $COUNTER seconds more `date` > tmp.log  # tmp.log contains last entry
-    sleep 1
+	z=$(($COUNTER / 60))
+
+
+if [ $COUNTER -lt 0 ]
+then
+    echo Predicting task  $2  to require  $1 minutes but took $COUNTER seconds MORE $z min  `date` > tmp.log  #
+elif [ $COUNTER -gt 0 ]
+then
+    echo Predicting task  $2  to require  $1 minutes but took $COUNTER seconds or $z min LESS `date` > tmp.log  # 
+else
+    echo BULLSEYEPrediction of   $2  to require  $1  :: `date` > tmp.log  # 
+fi
+
+sleep 1
 done
 
 #echo $COUNTER   # both these lines are not shown 
