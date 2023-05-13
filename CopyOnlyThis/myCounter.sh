30173c93e8a63a952772a0ec152f54e4d3839dc9
Sunil Choudhary <sunil.choudhary@gmail.com>
2023-05-13 12:24:53 +0530

    Update myCounter.sh
    some formating

--- CopyOnlyThis/myCounter.sh
+++ CopyOnlyThis/myCounter.sh
@@ -4,6 +4,7 @@ COUNTER=$1
 TASKNOTE=$2
 COUNTER=$(( COUNTER * 60 ))
 PREDICTED=$COUNTER
+touch tmp.log 
 cat tmp.log >> forecast.log   # Concatenate to old logs 
 while true
 do
@@ -14,12 +15,12 @@ do
 
 if [ $COUNTER -lt 0 ]
 then
-    echo Predicting task  $2  to require  $1 minutes but took $COUNTER seconds MORE $z min  `date` > tmp.log  #
+    echo Predicting task  $2  to require  $1 minutes but took $COUNTER seconds MORE $z min on `date` > tmp.log  #
 elif [ $COUNTER -gt 0 ]
 then
-    echo Predicting task  $2  to require  $1 minutes but took $COUNTER seconds or $z min LESS `date` > tmp.log  # 
+    echo Predicting task  $2  to require  $1 minutes but took $COUNTER seconds LESS $z min on `date` > tmp.log  # 
 else
-    echo BULLSEYEPrediction of   $2  to require  $1  :: `date` > tmp.log  # 
+    echo BULLSEYE, Prediction of   $2  to require  $1 Good Job Sunil on  :: `date` > tmp.log  # 
 fi
 
 sleep 1
