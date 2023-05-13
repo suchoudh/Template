2716f30590982c16fefecb490906cd9ff78b2c46
Sunil Choudhary <sunil.choudhary@gmail.com>
2023-05-13 17:15:17 +0530

    Update useme.sh
    git clone

--- useme.sh
+++ useme.sh
@@ -1,7 +1,8 @@
-export DATETAG="$(date +'%Y_%m_%d_%H_%M')"
-export DIRNAME=$DATETAG"_AUTOv1"
-echo $DIRNAME
-mkdir $DIRNAME
-cp -R ~/Documents/Template/CopyOnlyThis "$DIRNAME"/.
+export DATETAG="$(date +'%Y_%m_%d_%H_%M_%S')"
+
+export DIRNAME="$(date +'%Y_%m_%d_%H_%M_%S')_AUTO"
+
+git clone https://www.github.com/suchoudh/Template.git 
+mv Template $DIRNAME
 touch "$DIRNAME"/"readme$DATETAG.md"
-cat ~/Documents/Template/CopyOnlyThis/readmeClone.md >>$DIRNAME/"readme$DATETAG.md"
+cat "$DIRNAME"/"CopyOnlyThis"/"readmeClone.md" >> "$DIRNAME"/"readme$DATETAG.md"  
