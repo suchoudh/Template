415f5c7ab0acb3569724e3ee4456d61dff7385cc
Sunil Choudhary <sunil.choudhary@gmail.com>
2023-05-20 10:07:02 +0530

    Update useme.sh
    move out files to be used 
    remove CopyThis folder (purge it after august 2023 )

--- useme.sh
+++ useme.sh
@@ -1,8 +1,13 @@
 export DATETAG="$(date +'%Y_%m_%d_%H_%M_%S')"
 
-export DIRNAME="$(date +'%Y_%m_%d_%H_%M_%S')_AUTO"
+export DIRNAME="$(date +'%Y_%m_%d_%H_%M_%S')_Lenovo"
 
 git clone https://www.github.com/suchoudh/Template.git 
+sleep 2
 mv Template $DIRNAME
 touch "$DIRNAME"/"readme$DATETAG.md"
+mv $DIRNAME/CopyOnlyThis/warikoToday.ods $DIRNAME/warikoo$DATETAG.ods
+mv $DIRNAME/CopyOnlyThis/myCounter.sh $DIRNAME
+mv $DIRNAME/CopyOnlyThis/CornellTemplate.ott $DIRNAME
+rm $DIRNAME/CopyThis
 cat "$DIRNAME"/"CopyOnlyThis"/"readmeClone.md" >> "$DIRNAME"/"readme$DATETAG.md"  
