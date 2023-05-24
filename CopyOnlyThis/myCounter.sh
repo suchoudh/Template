a68c134cf88f6e5da495e8af28707b57afa88545
Sunil Choudhary <sunil.choudhary@gmail.com>
2023-05-24 11:28:04 +0530

    Update myCounter.sh
    # within a min is bulls eye

--- CopyOnlyThis/myCounter.sh
+++ CopyOnlyThis/myCounter.sh
@@ -13,10 +13,10 @@ do
 	z=$(($COUNTER / 60))
 
 
-if [ $COUNTER -lt 0 ]
+if [ $COUNTER -lt 60 ]    # within a min is bulls eye 
 then
     echo Predicting task  $2  to require  $1 minutes but took $COUNTER seconds MORE $z min on `date` > tmp.log  #
-elif [ $COUNTER -gt 0 ]
+elif [ $COUNTER -gt 60 ]
 then
     echo Predicting task  $2  to require  $1 minutes but took $COUNTER seconds LESS $z min on `date` > tmp.log  # 
 else
