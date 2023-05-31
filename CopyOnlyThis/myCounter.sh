945c6b27bbe486bd678843b17493c5aa620139ae
Sunil Choudhary <sunil.choudhary@gmail.com>
2023-05-31 13:03:31 +0530

    Update myCounter.sh
    perfecting Actual vs Predicted

--- CopyOnlyThis/myCounter.sh
+++ CopyOnlyThis/myCounter.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-
 COUNTER=$1
 TASKNOTE=$2
 PREDICTED=$1
@@ -9,24 +6,21 @@ touch tmp.log
 cat tmp.log >> forecast.log   # Concatenate to old logs 
 while true
 do
-	echo $COUNTER seconds remaining in break
+	echo $COUNTER seconds remaining in $TASKNOTE for mandatory break 
 	COUNTER=$(( COUNTER - 1 ))
 	z=$(($COUNTER / 60))
 	Actual=$(($PREDICTED-$z))
-	echo $Actual
+#	echo $Actual
 	
 if [ $COUNTER -lt -60 ]    # within a min is bulls eye 
 then
-    echo Predicting task  $2  to require  $1 minutes but took $COUNTER seconds MORE $z min on `date` > tmp.log  #
+    echo Predicted time for  $TASKNOTE :$1  Actual :: $Actual ..  took $COUNTER seconds MORE  on `date` > tmp.log  #
 elif [ $COUNTER -gt 60 ]
 then
-    echo Predicting task  $2  to require  $1 minutes but took $COUNTER seconds LESS $z min on `date` > tmp.log  # 
+    echo Predicting time for $TASKNOTE : $1  Actual :: $Actual ...  took $COUNTER seconds LESS on `date` > tmp.log  # 
 else
     echo BULLSEYE, Prediction of   $2  to require  $1 Good Job Sunil on  :: `date` > tmp.log  # 
 fi
 
 sleep 1
 done
-
-#echo $COUNTER   # both these lines are not shown 
-#echo $2  to require this time.$1 deviation was $COUNTER 
