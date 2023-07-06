6c2d19f7ed4a6ffa55b21fd6e4409c36eedf41a1
Sunil Choudhary <sunil.choudhary@gmail.com>
2023-07-06 12:28:07 +0530

    press q or Q to Quit  the script
    here can now add the final flush as well. . from tmp which is like a elephants tail being stuck till next time. 
    :) ha ha ha

--- CopyOnlyThis/myCounter.sh
+++ CopyOnlyThis/myCounter.sh
@@ -25,4 +25,12 @@ else
 fi
 
 sleep 1
+
+# In the following line -t for timeout, -N for just 1 character
+    read -t 0.25 -N 1 input
+    if [[ $input = "q" ]] || [[ $input = "Q" ]]; then
+# The following line is for the prompt to appear on a new line.
+        echo
+        break 
+    fi
 done
