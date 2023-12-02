322a8ed51f8a538fecbf8296d8086cddc6dcae17
Sunil Choudhary <sunil.choudhary@gmail.com>
2023-12-02 12:35:26 +0530

    Updated myCounter.sh in CopyOnlyThis
    as this was overwriting the one in home

--- CopyOnlyThis/myCounter.sh
+++ CopyOnlyThis/myCounter.sh
@@ -1,11 +1,22 @@
-
-# Add the header ... !!! SVP 
-
-
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 PURPLE='\033[0;35m'
 NC='\033[0m' # No Color
+
+## For reference Only 
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+
+# Log Format for forecast.log (to be done 30 nov 2023 ) 
+# Remark :Good Job Sunil | Task : Lunch | Estimate : 26 min | Actual: 27 min | Deviation : + 1 Min | % :  3% more 
+# Remark :UnderEstimated  | Task : Lunch | Estimate : 26 min | Actual: 66 min | Deviation : + 40 Min | % :  76% more time 
+# Remark :OverEstimated | Task : Lunch | Estimate : 26 min | Actual: 16 min | Deviation : - 4 Min | % :  15% less time 
 
 #Region --- Updates
 #    echo -e "${PURPLE}RUNNING: sudo apt update --fix-missing${NC}"
@@ -19,7 +30,7 @@ PREDICTED=$1
 COUNTER=$(( COUNTER * 60 ))
 touch tmp.log 
 cat tmp.log >> forecast.log   # Concatenate to old logs 
-cat tmp.log >> $LOGGER/forecast.log   # Concatenate to logger file as well  
+#cat tmp.log >> $LOGGER/forecast.log   # Concatenate to logger file as well  
 while true
 do
 #	echo $COUNTER seconds remaining in $TASKNOTE for mandatory break :: press q/Q to quit  # this line colour to change based on 
@@ -31,14 +42,14 @@ do
 	
 if [ $COUNTER -lt -60 ]    # within a min is bulls eye 
 then
-    echo -e "${RED} $Percentage % elapsed time for TASK $TASKNOTE. Used $Actual minutes MORE from Allocated $1 minutes.. took $COUNTER secs MORE on `date` ${NC}" 
+    echo -e "${RED} $Percentage MORE predicted time for tasknote $TASKNOTE:$1 Actual ::$Actual .. took $COUNTER secs MORE on `date` ${NC}" 
     echo $Percentage percent MORE Predicted time for  $TASKNOTE :$1  Actual :: $Actual ..  took $COUNTER seconds MORE  on `date` > tmp.log  #
 elif [ $COUNTER -gt 60 ]
 then
-    echo -e "${GREEN} $Percentage % elapsed time  for Task $TASKNOTE. Used $Actual minutes of Allocated ::$1 minutes..took $COUNTER secs LESS on `date` ${NC}" 
+    echo -e "${GREEN} $Percentage MORE predicted time for tasknote $TASKNOTE:$1 Actual ::$Actual .. took $COUNTER secs MORE on `date` ${NC}" 
     echo $Percentage percent LESS Predicting time for $TASKNOTE : $1  Actual :: $Actual ...  took $COUNTER seconds LESS on `date` > tmp.log  # 
 else
-    echo -e "${PURPLE} $Percentage % elapsed time for Task $TASKNOTE. Used $Actual  minutes of Allocated $1 minutes .. Good Job Sunil  on ::  `date` ${NC}" 
+    echo -e "${PURPLE} $Percentage percent BULLSEYE, Prediction of $2 to require $1 Good Job Sunil on ::  `date` ${NC}" 
     echo $Percentage percent BULLSEYE, Prediction of   $2  to require  $1 Good Job Sunil on  :: `date` > tmp.log  # 
 fi
 
@@ -49,9 +60,6 @@ sleep 1
     if [[ $input = "q" ]] || [[ $input = "Q" ]]; then
 # The following line is for the prompt to appear on a new line.
         echo
-	cat tmp.log >> forecast.log   # Concatenate to old logs 
-	cat tmp.log >> $LOGGER/forecast.log   # Concatenate to logger file as well  
-	echo --- flushed at `date`---  >tmp.log 
         break 
     fi
 done
