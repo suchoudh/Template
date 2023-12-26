235f161ea52415d7572d2ce06520660ee2fd2e2b
Sunil Choudhary <sunil.choudhary@gmail.com>
2023-12-26 23:26:37 +0530

    Dropbox integration and Offline working as wel

--- CopyOnlyThis/useme.sh
+++ CopyOnlyThis/useme.sh
@@ -1,13 +1,14 @@
 #~~ ~~~~~WeekNumber is 32~~~~~***useme.sh ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~   File                : useme.sh 
 #~~   Creation DateTime   :32_Tue_20230808-11_24_37
-#~~   Last Updated Date   :2023-11-24 Fri 01:06 PM 
+#~~   Last Updated Date   :2023-12-26 
 #~~   End Date	
 #~~   Author              : Sunil Choudhary 
 #~~   Remarks             : CopyFilesFromGitorUseDesktopTemplate 
 #~~   1: 2023-11-03 Fri 09:03 AM - cd to $DIRNAME as last step  
 #a~~  2: 2023-11-24 Fri 12:44 PM - create in day with name as myToday and move to document with older name the offline is yet to be done 
-#a~~  3: 2023-11-27 Mon 11:28 AM-- yesterday corrected 
+#a~~  3: 2023-12-26 		-- offline working from Template as well. 
+#a~~  4: Done: on 26 dec 2023:    Concept: 2023-12-15 Fri 03:58 PM-- keep files in dropbox and set softlink on desktop  Done 
 #~~ ~~~~~~~~111~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #aaaa
 #### Set env ###  
@@ -17,8 +18,8 @@ export DATETAG="$(date +'%Y_Wk%W_%m_%d')"
 export YESTERDAY=`date --date "-1 days"  +"%Y_Wk%W_%m_%d"`
 export DATETAG=$YESTERDAY # as myToday is shifted to documents on next day morning and not today 
 export DIRNAME=$DATETAG"_myToday"  # myDate is todays date it should be yesterday date ideally. 
-export MYTODAY="myToday"
-
+export MYTODAY="Doday"
+export MYTODAY_HOME='/home/rahu/Desktop/Dropbox'  #Shift this to Dropbox and keep a link on Desktop 
 cd ~/    # Run this script from home directory always and not from ~/Desktop/$MYTODAY 
 
 #### Check Net is ( on : copy from github off: copy from template ###  
@@ -46,15 +47,15 @@ if [[ $? -eq 0 ]]; then
 	sleep 2 
 	cat tmp.log >>forecast.log # this flushes the last entry 
 	echo "====LAST FLUSH====" >> forecast.log
-	cd ~/Desktop  # get back to desktop 
-#	mv 2023* ~/Documents/
-	echo $DIRNAME
+	cd $MYTODAY_HOME  # get back to root  
+#	mv 2023* ~/Documents/ 
+	echo $DIRNAME 
 	mv -i $MYTODAY ~/Documents/$DIRNAME
 	mv -f Template* ~/Documents/    # Flush  older template to documents as new is getting downloaded.  
 
 	git clone https://www.github.com/suchoudh/Template.git 
 	sleep 2
-	cp -r Template ~/Desktop/Template$DATETAG
+	cp -r Template $MYTODAY_HOME/Template$DATETAG
 	mv -i Template $MYTODAY
 	touch "$MYTODAY"/"readme$DATETAG.md"
 	mv $MYTODAY/CopyOnlyThis/warikoToday.ods $MYTODAY/warikoo$DATETAG.ods
@@ -63,12 +64,13 @@ if [[ $? -eq 0 ]]; then
 	cat "$MYTODAY"/"CopyOnlyThis"/"readmeClone.md" >> "$MYTODAY"/"readme$DATETAG.md"  
 # --------------------------------------------------------------
 else
-        echo "you appear to be Offline .. Copying from Template"  # Use Template if Offline 
-	cd ~/Desktop
-	mv 2023* ~/Documents/    # cleanup older to documents folder. 
+        echo "you appear to be Offline .. Copy from Template"  # Use Template if Offline 
+#	exit  # break the logic here itself. 
+	cd $MYTODAY_HOME
+	mv 2023* ~/Documents/    # cleanup older to documents folder. Change to 2024 next week  
 	export DATETAG="$(date +'%Y_%m_%d_%H_%M_%S')"
-	export DIRNAME="$(date +'%Y_%m_%d_%H_%M_%S')_Offline"
-	cp -r ~/Desktop/Template* ~/Desktop/$DIRNAME   # Copy from latest Template # What happens when there are more Template_DATE files ?? TBD 
+	export DIRNAME="$(date +'%Y_%m_%d_%H_%M_%S')_Offline_DBox"
+	cp -r $MYTODAY_HOME/Template* $MYTODAY_HOME/$DIRNAME   # Copy from latest Template # What happens when there are more Template_DATE files ?? TBD 
 #	mkdir $DIRNAME   # Not needed 
 	touch "$DIRNAME"/"readme$DATETAG.md"
 	mv $DIRNAME/CopyOnlyThis/warikoToday.ods $DIRNAME/warikoo$DATETAG.ods
@@ -77,8 +79,11 @@ else
 	cat "$DIRNAME"/"CopyOnlyThis"/"readmeClone.md" >> "$DIRNAME"/"readme$DATETAG.md"  
 fi
 
-	cd ~/Desktop/$MYTODAY
+
 
 echo BoD HighMark was 
 cat /home/rahu/Desktop/Dropbox/3400_Routine/myData/_BoD/BoD.HighMark
+cd $MYTODAY_HOME/$MYTODAY
+# cd /home/rahu/Desktop/Dropbox/Doday
+chmod +x 6m_24m.sh
 #~~ Ran on 32_Tue_20230808-11_24_37
